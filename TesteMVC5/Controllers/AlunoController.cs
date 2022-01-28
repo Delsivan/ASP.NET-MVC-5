ea3cf95ebe833e1216518c2c89a44d354c5cac0b
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2022-01-28 16:22:47 -0300

    Adicionando a controller AlunoController

---
+++ TesteMVC5/Controllers/AlunoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesteMVC5.Models;
+
+namespace TesteMVC5.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno 
+        // estar pedindo uma View
+        [HttpGet]
+        [Route(template:"novo-aluno")]
+        public ActionResult NovoAluno()
+        {
+            return View();
+        }
+
+        // POST: Aluno
+        // Recebe os dados 
+        [HttpPost]
+        [Route(template: "novo-aluno")]
+        public ActionResult NovoAluno(Aluno aluno)
+        {
+            if (!ModelState.IsValid) return View(aluno);
+            return View(aluno);
+        }
+    }
+}
