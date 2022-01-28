717c661eb22e06e805f67cbbcb48bef650fd77a0
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2022-01-28 16:21:04 -0300

    Adicionando a classe (model) Aluno

---
+++ TesteMVC5/Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TesteMVC5.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string Nome { get; set; }
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string CPF { get; set; }
+
+        public DateTime DataMatricula { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
