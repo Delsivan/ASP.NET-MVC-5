4a06b7e6fda26744160d90a67b19169413f03319
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2022-02-27 13:41:19 -0300

    Connection Strings

--- TesteMVC5/Data/AppDbContext.cs
+++ TesteMVC5/Data/AppDbContext.cs
@@ -1,9 +1,15 @@
 ﻿using System.Data.Entity;
-
+using TesteMVC5.Models;
 
 namespace TesteMVC5.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
     }
 }
